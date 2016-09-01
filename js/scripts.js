555e5cf3c52f1af3de8bf52f29935e4174f29579
Sheena Nickerson <sheenanick@gmail.com>
2016-09-01 15:17:34 -0700

    refactor js by creating a Survey object and selectTrack method

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,80 @@
+function Survey() {
+  this.name;
+  this.frontEnd;
+  this.appType;
+  this.companySize;
+  this.javaOrC;
+}
+
+Survey.prototype.selectTrack = function() {
+  var trackName;
+  var trackID;
+  if (this.frontEnd === "front-end1" || this.frontEnd === "front-end2") {
+    trackName = "CSS/Design";
+    trackID = "#css";
+  } else if (this.appType === "mobile") {
+    trackName = "Java/Android";
+    trackID = "#java";
+  } else if (this.companySize === "small") {
+    trackName = "Ruby/Rails";
+    trackID = "#ruby";
+  } else if (this.javaOrC === "cSharp") {
+    trackName = "C#/.NET";
+    trackID = "#c-sharp";
+  } else {
+    trackName = "Java/Android";
+    trackID = "#java";
+  }
+  return [trackName, trackID];
+}
+
 $(document).ready(function(){
-  $("form").submit(function(){
-    var name = $("input#name").val();
-    var frontEnd = $("#front-end").val();
-    var appType = $("input:radio[name=app-type]:checked").val();
-    var companySize = $("input:radio[name=size]:checked").val();
-    var javaOrC = $("#java-c").val();
-
-    $(".results #greeting, #track-name, #css, #java, #ruby, #c-sharp").hide();
-
-    $(".results #input-name").text(name);
-
-
-    if (name && frontEnd && appType && companySize && javaOrC) {
-      $(".results #greeting").show();
-      if (frontEnd === "front-end1" || frontEnd === "front-end2") {
-        generateOutput("CSS/Design", "#css");
-      } else if (appType === "mobile") {
-        generateOutput("Java/Android", "#java");
-      } else if (companySize === "small") {
-        generateOutput("Ruby/Rails", "#ruby");
-      } else if (javaOrC === "cSharp") {
-        generateOutput("C#/.NET", "#c-sharp");
-      } else {
-        generateOutput("Java/Android", "#java");
-      }
-    }
-    function generateOutput(trackName, trackId) {
-      $(".results #track-name").text('"' + trackName + '"').show();
-      $(trackId).show();
-    }
-    event.preventDefault();
+  var survey = new Survey;
+  $("button#start").click(function(event){
+    $(".title-page").hide();
+    $("form#question1").show();
   });
+  $("form#question1").submit(function(){
+    event.preventDefault();
+    survey.name = $("input#name").val();
+    $("form#question1").hide();
+    $("form#question2").show();
+  });
+  $("form#question2").submit(function(){
+    event.preventDefault();
+    survey.frontEnd = $("input:radio[name=front-end]:checked").val();
+    $("form#question2").hide();
+    $("form#question3").show();
+  });
+  $("form#question3").submit(function(){
+    event.preventDefault();
+    survey.appType = $("input:radio[name=app-type]:checked").val();
+    $("form#question3").hide();
+    $("form#question4").show();
+  });
+  $("form#question4").submit(function(){
+    event.preventDefault();
+    survey.companySize = $("input:radio[name=size]:checked").val();
+    $("form#question4").hide();
+    $("form#question5").show();
+  });
+  $("form#question5").submit(function(){
+    event.preventDefault();
+    survey.javaOrC = $("#java-c").val();
+    $("form#question5").hide();
+    generateOutput(survey.selectTrack());
+    $("#greeting, .restart-button").show();
+    $("#insert-name").text(survey.name);
+  });
+  $("#restart").click(function(event){
+    $("#greeting, #track-name, #css, #java, #ruby, #c-sharp, .restart-button").hide();
+    $(".title-page").show();
+    $("form#question1")[0].reset();
+  });
+
+  function generateOutput([trackName, trackId]) {
+    $("#track-name").text('"' + trackName + '"').show();
+    $(trackId).show();
+  }
+
 });
